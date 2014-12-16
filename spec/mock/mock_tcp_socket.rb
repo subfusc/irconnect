571f8cb0edb30a3fcc8b1d2733e142059d555b1e
Justin Speers <speersjustin@fastmail.fm>
2014-12-16 12:54:13 -0800

    refactor mock tcp socket

--- spec/mock/mock_tcp_socket.rb
+++ spec/mock/mock_tcp_socket.rb
@@ -1,20 +1,18 @@
 # phony TCPSocket for testing purposes
 class MockTCPSocket
-  @@server_responses  = []
-  @@client_output     = String.new
-  
-  # take the same parameters as the TCPSocket constructor
-  # for the sake of consistency; they are not used
-  def initialize(host, port)
+  attr_reader :client_output
+
+  def initialize(*)
+    @server_responses  = []
+    @client_output     = ''
   end
 
   def gets
-    raise "No response from server" if @@server_responses.empty?
-    @@server_responses.shift + "\r\n"
+    fail 'No response from server' if @server_responses.empty?
+    @server_responses.shift + "\r\n"
   end
 
   def print(output)
-    @@client_output << output
+    @client_output << output
   end
-
 end
